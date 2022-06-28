12f5c321f8c97892b299e223b3b15bc61ad85c11
DL-Berkey <ruyria@naver.com>
2022-06-28 21:46:09 +0900

    create addButton-function

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 let addButton = document.getElementById("add_button");
 let memoList = document.getElementById("memo_list");
 
-addButton.addEventListener("click", () => {
+function createMemoTitle(container) {
+    let memoTitle = document.createElement("h4");
+
+    memoTitle.style.marginLeft = "20px";
+    memoTitle.style.float = "left";
+    memoTitle.innerHTML = "메모 " + String(parseInt(container.childElementCount) + 1);
+
+    return memoTitle;
+}
+
+function createEditIcon() {
+    editIcon = document.createElement("i");
+
+    editIcon.setAttribute("class", "fa-solid fa-pen");
+    editIcon.style.color = "#94B49F";
+
+    return editIcon;
+}
+
+function createEditButton(editIcon) {
+    let editButton = document.createElement("button");
+
+    editButton.style.all = "unset";
+    editButton.style.float = "right";
+    editButton.style.marginRight = "15px"
+
+    editButton.appendChild(editIcon);
+
+    return editButton;
+}
+
+
+function createMemo(container) {
     let memo = document.createElement("div");
-    memo.innerHTML = "추가된거";
-    memo.style.borderBottom = "2px solid #FCF8E8"
-    memo.style.marginTop = "5px"
+    let memoTitle = createMemoTitle(container);
+    let editIcon = createEditIcon();
+    let editButton = createEditButton(editIcon);
 
-    memoList.appendChild(memo);
+    memo.style.height = "42px";
+    memo.style.borderBottom = "2px solid #FCF8E8";
+    memo.style.marginTop = "5px";
+    memo.style.position = "relative";
 
-    if(memoList.childElementCount == 16) {
-        memoList.style.overflow = "scroll"
+    memo.appendChild(memoTitle);
+    memo.appendChild(editButton);
+
+    container.appendChild(memo);
+
+    if(container.childElementCount == 14) {
+        container.style.overflow = "scroll"
     }
+}
+
+
+addButton.addEventListener("click", () => {
+    createMemo(memoList);
 })
